d7a85a7e84bc48ad60e5ac6cf16dc3f67e690212
aechchiki <a.echchiki@gmail.com>
2018-08-16 08:40:49 +0200

    added junctions eval

--- sam_to_gtf.sh
+++ sam_to_gtf.sh
@@ -6,16 +6,18 @@
 # map transcripts in fasta format to the reference genome
 minimap2 -ax splice ref-genome.fasta transcripts.fasta > alignment.sam
 
+# keep uniquely mapped in sam
 # remove unmapped & multimappings
 cat alignment.sam  | grep -v ^@ | awk '{if($2!=4 && $2!=256 && $2!=272 && $2!=2048 && $2!=2064){print$0}}' > seen_once.sam
-# verify that you removed all but forward/reverse
+# verify that you removed all but forward/reverse -- should only display 0, 16 flags
 cat seen_once.sam | cut -f2 | sort | uniq -c
-# should only display 0, 16 flags
-
 # save header
 cat alignment.sam | grep ^@ > sam.header
 # merge header & forward/reverse mapped
 cat sam.header seen_once.sam > seen_once_header.sam
+
+# for later: extract junctions
+/home/aechchik/software/minimap2/misc/paftools.js junceval -e /scratch/beegfs/monthly/aechchik/isoforms/ref/Drosophila_melanogaster.BDGP6.84.gtf seen_once_header.sam > junctions.txt
 
 # convert sam to bam
 module add UHTS/Analysis/samtools/1.3
@@ -34,4 +36,4 @@ samtools sort -T . -o seen_once_header_sort.bam seen_once_header.bam
 cat seen_once_header_sort.gff2 | sed 's/mRNA/transcript/g' | sed 's/transcript /transcript_id "/g' | sed 's/exon /transcript_id "/g' | sed 's/;/";/g' > seen_once_header_sort.gtf
 
 # compare to ref
-/home/aechchik/bin/cuffcompare -G seen_once_header_sort.gtf -r /scratch/beegfs/monthly/aechchik/isoforms/Drosophila_melanogaster.BDGP6.39.gtf
+/home/aechchik/bin/cuffcompare -G seen_once_header_sort.gtf -r /scratch/beegfs/monthly/aechchik/isoforms/ref/Drosophila_melanogaster.BDGP6.39.gtf
