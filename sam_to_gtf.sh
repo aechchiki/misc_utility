e76bd4791c23a896193ce62583dc0ba3d662d554
aechchiki <a.echchiki@gmail.com>
2018-08-16 08:36:38 +0200

    compare experimental transcripts to reference annotation

---
+++ sam_to_gtf.sh
@@ -0,0 +1,37 @@
+# idea: map transcripts to reference genome (minimap2, sam; or asm, gtf) and convert the alignment to compare with the ref annotation (gtf)
+
+# but first: sometimes the assemblies are reported in gtf format. need to convert to fasta before re-mapping to reference genome
+/home/aechchik/bin/gffread transcripts.gtf -g ref-genome.fasta -w transcripts.fasta
+
+# map transcripts in fasta format to the reference genome
+minimap2 -ax splice ref-genome.fasta transcripts.fasta > alignment.sam
+
+# remove unmapped & multimappings
+cat alignment.sam  | grep -v ^@ | awk '{if($2!=4 && $2!=256 && $2!=272 && $2!=2048 && $2!=2064){print$0}}' > seen_once.sam
+# verify that you removed all but forward/reverse
+cat seen_once.sam | cut -f2 | sort | uniq -c
+# should only display 0, 16 flags
+
+# save header
+cat alignment.sam | grep ^@ > sam.header
+# merge header & forward/reverse mapped
+cat sam.header seen_once.sam > seen_once_header.sam
+
+# convert sam to bam
+module add UHTS/Analysis/samtools/1.3
+samtools view -bS seen_once_header.sam > seen_once_header.bam
+
+# sort bam
+samtools sort -T . -o seen_once_header_sort.bam seen_once_header.bam
+
+# convert to bed
+/home/aechchik/bin/bamToBed -bed12 -i seen_once_header_sort.bam > seen_once_header_sort.bed
+
+# convert to gff2
+/home/ogustari/galaxy/tools/filters/bed_to_gff_converter.py seen_once_header_sort.bed seen_once_header_sort.gff2
+
+# convert to cufflinks-like gtf
+cat seen_once_header_sort.gff2 | sed 's/mRNA/transcript/g' | sed 's/transcript /transcript_id "/g' | sed 's/exon /transcript_id "/g' | sed 's/;/";/g' > seen_once_header_sort.gtf
+
+# compare to ref
+/home/aechchik/bin/cuffcompare -G seen_once_header_sort.gtf -r /scratch/beegfs/monthly/aechchik/isoforms/Drosophila_melanogaster.BDGP6.39.gtf
